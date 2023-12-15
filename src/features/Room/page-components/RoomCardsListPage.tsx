86cd4b5c089b453007d4e2c4c10ba0816c64ef7d
1718wani <ikuya1293@gmail.com>
2023-12-15 09:41:58 +0900

    ✨ RoomCardsListPageComponentをScrollViewにしました

--- src/features/Room/page-components/RoomCardsListPage.tsx
+++ src/features/Room/page-components/RoomCardsListPage.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 
 import { MakeRoomButton } from "../components/MakeRoomButton";
 import { RoomCard } from "../components/RoomCard";
@@ -6,9 +6,14 @@ import { RoomCard } from "../components/RoomCard";
 export const RoomCardsListPageComponent = () => {
   return (
     <View className="flex-1 pt-4">
-      <View className="flex-1">
-        <RoomCard />
-      </View>
+      <ScrollView>
+        <View className="flex-1">
+          <RoomCard />
+          <RoomCard />
+          <RoomCard />
+          <RoomCard />
+        </View>
+      </ScrollView>
       <View className="absolute bottom-10 w-full items-center">
         <View className="h-1/3">
           <MakeRoomButton />

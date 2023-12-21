aeab9930a5d3bb283eaefcb71d0c57cd40dfc560
1718wani <ikuya1293@gmail.com>
2023-12-22 07:52:13 +0900

    ✨ 新しいルーティングファイルを作成しました

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,16 @@
-import { Link, Slot, Stack } from "expo-router";
+import { Link, Slot, Stack, useNavigation, usePathname } from "expo-router";
 
-import { Image, Pressable } from "react-native";
+import { Image } from "expo-image";
+import { Pressable, View } from "react-native";
 import { path } from "../consts/path";
 import { UserAvatorButton } from "../features/User/components/UserAvatorButton";
 import { RootSiblingParent } from "react-native-root-siblings";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { RoomStatusOrStopButton } from "../features/Room/components/room-detail-components/RoomStatusOrStopButton";
 
 export default function HomeLayout() {
+  const router = usePathname();
+  const shouldShowButton = router === "/" || router === "/timeline";
   return (
     <>
       <RootSiblingParent>
@@ -38,6 +42,8 @@ export default function HomeLayout() {
             options={{
               title: "ルーム一覧",
               headerRight: () => <UserAvatorButton />,
+
+              headerBackVisible: false,
             }}
           />
 
@@ -46,10 +52,31 @@ export default function HomeLayout() {
             options={{ presentation: "modal", title: "ルームを作成する" }}
           />
           <Stack.Screen
+            name="edit-room-rule"
+            options={{ presentation: "modal", title: "ルールを変える" }}
+          />
+          <Stack.Screen
+            name="approve-room-rule"
+            options={{ presentation: "modal", title: "ルールを承認する" }}
+          />
+          <Stack.Screen
+            name="first-approve-room-rule"
+            options={{
+              presentation: "modal",
+              title: "入室前にルールを確認する",
+            }}
+          />
+          <Stack.Screen
             name="edit-user"
             options={{ presentation: "modal", title: "ユーザー情報" }}
           />
         </Stack>
+
+        {shouldShowButton && (
+          <View className="absolute bottom-0 mb-5 ml-40  ">
+            <RoomStatusOrStopButton />
+          </View>
+        )}
       </RootSiblingParent>
     </>
   );

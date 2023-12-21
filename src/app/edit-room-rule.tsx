2279431228a163515b72e773aa89ee3f2269134e
1718wani <ikuya1293@gmail.com>
2023-12-22 07:54:44 +0900

    ✨ 新しいルーティングファイルをapp直下に3つ作成しました

---
+++ src/app/edit-room-rule.tsx
@@ -0,0 +1,5 @@
+import { CreateRoomPageComponent } from "../features/Room/page-components/CreateRoomPage";
+
+export default function EditRoomRulePage() {
+    return <CreateRoomPageComponent />;
+  }

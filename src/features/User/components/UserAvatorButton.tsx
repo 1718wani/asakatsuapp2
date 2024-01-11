e160a7b91dda5e7a2fe40d7f637ba409a3a610b0
1718wani <ikuya1293@gmail.com>
2024-01-11 18:58:48 +0900

    💄 userInfoをローディングしているときのskeltonを追加しました

--- src/features/User/components/UserAvatorButton.tsx
+++ src/features/User/components/UserAvatorButton.tsx
@@ -1,20 +1,39 @@
 import React from "react";
 import { Link } from "expo-router";
-import { Pressable } from "react-native";
+import { Pressable, View } from "react-native";
 import { path } from "../../../consts/path";
 import { Image } from "expo-image";
+import useSWR from "swr";
+import { getUserInfo } from "../../Room/apis/getUserInfo";
+import Toast from "react-native-toast-message";
+import { Skeleton } from "@rneui/themed";
 
 export const UserAvatorButton = () => {
+  const {
+    data: userInfo,
+    isLoading: userInfoLoading,
+    error: userInfoError,
+  } = useSWR(["userInfo"], () => getUserInfo());
+
   return (
-    <Link href={path.editUser} asChild>
-      <Pressable>
-        {() => (
-          <Image
-            source={{ uri: "https://i.pravatar.cc/300" }}
-            className="rounded-full w-9 h-9"
-          />
-        )}
-      </Pressable>
-    </Link>
+    <>
+      {userInfoLoading && (
+        <>
+          <Skeleton circle animation="wave" width={30} height={30} />
+        </>
+      )}
+      {userInfo && (
+        <Link href={path.editUser} asChild>
+          <Pressable>
+            {() => (
+              <Image
+                source={{ uri: userInfo?.avatar_url }}
+                className="rounded-full w-9 h-9"
+              />
+            )}
+          </Pressable>
+        </Link>
+      )}
+    </>
   );
 };

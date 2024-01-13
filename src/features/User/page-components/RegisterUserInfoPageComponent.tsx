8f3a202ab5321d2fa234e43c9aa614e23495db73
1718wani <ikuya1293@gmail.com>
2024-01-13 23:02:49 +0900

    🔥 不必要なImport文を削除しました

--- src/features/User/page-components/RegisterUserInfoPageComponent.tsx
+++ src/features/User/page-components/RegisterUserInfoPageComponent.tsx
@@ -1,48 +1,31 @@
-import { Image } from "expo-image";
 import { useState } from "react";
-import {
-  Button,
-  Keyboard,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Clipboard from "expo-clipboard";
-import { MaterialIcons } from "@expo/vector-icons";
+import { View } from "react-native";
 import Toast from "react-native-toast-message";
-import { DisplayedUserCodeComponent } from "../components/DisplayedUserCodeComponent";
-import { Controller, useForm } from "react-hook-form";
-import { router } from "expo-router";
-import { updateProfile } from "../apis/updateProfile";
-import { useAtomValue } from "jotai";
-import { defaultRoomAtom } from "../../../states/defaultRoomAtom";
 import { useProfileForm } from "../hooks/useProfileForm";
 import { InitialProfileSubmitButtons } from "../components/InitialProfileSubmitButtons";
 import { ProfileForm } from "../components/ProfileFormComponent";
+import { defaultProfileImageUrl } from "../consts/defaultProfileImageUrl";
 
 export default function RegisterUserInfoPageComponent() {
-  const [image, setImage] = useState<string>(
-    "https://cdn.iconscout.com/icon/free/png-512/free-user-1851010-1568997.png?f=webp&w=512"
-  );
+  const [image, setImage] = useState<string>(defaultProfileImageUrl);
   const { handleSubmit, control, errors, onSubmit } = useProfileForm(image);
+
+  console.log(image, "現在セットされているイメージ");
 
   return (
     <>
       <View className=" mx-auto my-auto pb-40">
         <View className="mb-4">
-        <ProfileForm
-          control={control}
-          errors={errors}
-          image={image}
-          setImage={setImage}
-        />
+          <ProfileForm
+            control={control}
+            errors={errors}
+            image={image}
+            setImage={setImage}
+          />
         </View>
         <InitialProfileSubmitButtons onSubmit={handleSubmit(onSubmit)} />
       </View>
-      <Toast position="bottom"/>
-     
+      <Toast position="bottom" />
     </>
   );
 }

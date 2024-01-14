9bd3d4ac2e8dc0004518dfdca0b924d04dc0e9fa
1718wani <ikuya1293@gmail.com>
2024-01-14 20:17:34 +0900

    💄 roomListがから配列のとき、テキストを表示

--- src/features/Room/components/cardsComponent/RoomsListComponent.tsx
+++ src/features/Room/components/cardsComponent/RoomsListComponent.tsx
@@ -3,17 +3,20 @@ import Toast from "react-native-toast-message";
 import { Skeleton } from "@rneui/themed";
 import { getAllMyRoomsForRoomsListDisplay } from "../../apis/getAllMyRoomsForRoomsListDisplay";
 import { RoomCard } from "../RoomCard";
+import { Text } from "react-native";
 
 export const AllMyRoomsComponent = () => {
   const {
-    data: getAllMyRooms,
-    isLoading: getAllMyRoomsLoading,
-    error: getAllMyRoomsError,
+    data: AllMyRooms,
+    isLoading: AllMyRoomsLoading,
+    error: AllMyRoomsError,
   } = useSWR(["allMyJoinedRooms"], () => getAllMyRoomsForRoomsListDisplay());
+
+  console.log(AllMyRooms, "リストデータ");
 
   return (
     <>
-      {getAllMyRoomsLoading && (
+      {AllMyRoomsLoading && (
         <>
           {Array.from(Array(3).keys()).map((index) => (
             <Skeleton
@@ -25,25 +28,28 @@ export const AllMyRoomsComponent = () => {
           ))}
         </>
       )}
-      {getAllMyRoomsError &&
+      {AllMyRoomsError &&
         Toast.show({
           type: "error",
           text1: "参加しているルーム一覧が取得できませんでした",
         })}
-      {getAllMyRooms &&
-        getAllMyRooms.map(({ status, rooms, room_id }) => {
-          return (
-            <RoomCard
-              key={room_id}
-              roomId={room_id}
-              roomName={rooms?.name}
-              myStatus={status}
-              wakeUpTime={rooms?.rules?.wakeup_time}
-              purpose={rooms?.purpose}
-              participants={rooms?.room_members ?? []}
-            />
-          );
-        })}
+      {AllMyRooms && AllMyRooms.length > 0 ? (
+        AllMyRooms.map(({ status, rooms, room_id }) => (
+          <RoomCard
+            key={room_id}
+            roomId={room_id}
+            roomName={rooms?.name}
+            myStatus={status}
+            wakeUpTime={rooms?.rules?.wakeup_time}
+            purpose={rooms?.purpose}
+            participants={rooms?.room_members ?? []}
+          />
+        ))
+      ) : (
+        <Text className=" text-center mt-60 ">
+          現在表示されるルームはありません
+        </Text> // 空の場合に表示するテキスト
+      )}
     </>
   );
 };

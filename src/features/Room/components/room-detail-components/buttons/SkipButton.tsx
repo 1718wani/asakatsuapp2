dc61fcb84cfd1a9cbd9c1775654d7ea6dd28284d
1718wani <ikuya1293@gmail.com>
2024-02-11 22:45:11 +0900

    👔 Skipボタンの表示、非表示を切り替えるようにしました

--- src/features/Room/components/room-detail-components/buttons/SkipButton.tsx
+++ src/features/Room/components/room-detail-components/buttons/SkipButton.tsx
@@ -15,69 +15,65 @@ import { getDefaultRoomId } from "../../../apis/getDefaultRoomId";
 import { getRoomMembers } from "../../../apis/room_members/getRoomMembers";
 import { getRoomMember } from "../../../apis/room_members/getRoomMember";
 import { getUserId } from "../../../../User/apis/getUserId";
+import { SkipModal } from "../../../../AlarmClock/components/SkipModal";
+import { useQuery } from "@tanstack/react-query";
+import { getDefaultRoomInfo } from "../../../apis/getDefaultRoomInfo";
 
 export const SkipButton = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const handlePress = async () => {
-    // TODO これは開く前に判定するべき。あとで移す。
-    const defaultRoomId = await getDefaultRoomId();
-    const userId = await getUserId();
-    if (!defaultRoomId) return;
-    const skipLimit = (await getRule(defaultRoomId)).rules?.skip_limit;
-    const skipCount = (await getRoomMember(defaultRoomId, userId)).skip_count;
+  const [skipModalVisible, setSkipModalVisible] = useState(false);
 
-    if (skipLimit === skipCount) {
-    } else {
-      setModalVisible(true);
-    }
+  const {
+    data: defaultRoomInfo,
+    isLoading: defaultRoomInfoLoading,
+    error: defaultRoomInfoError,
+  } = useQuery({
+    queryKey: ["defaultRoomInfo"],
+    queryFn: () => getDefaultRoomInfo(),
+  });
+
+  const calculateRestOfSkipCount = (info: typeof defaultRoomInfo) => {
+    const skipLimit = info?.rules?.skip_limit ?? 5;
+    const skipCount = info?.room_members[0].skip_count ?? 0;
+    const diff = skipLimit - skipCount;
+    return diff;
   };
+
+  const restOfSkipCount = calculateRestOfSkipCount(defaultRoomInfo);
+
+  const handlePress = async () => {
+    setSkipModalVisible(true);
+  };
+
+  const isDisabled =
+    restOfSkipCount <= 0 ||
+    defaultRoomInfo?.room_members[0].status === "skipping";
+
   return (
     <>
-      <View className="flex-1 items-center justify-center mt-5">
-        <Modal
-          animationType="fade"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View className="flex-1 items-center justify-center mt-5">
-            <View className="m-5 w-2/3 h-1/3 bg-white rounded-xl p-9 items-center shadow-sm shadow-gray-300">
-              <Text className="mb-4 text-center">
-                スキップを一つ使いますか？
-              </Text>
-              <View className=" bottom-2 flex flex-row mx-2">
-                <Pressable
-                  className="rounded-lg px-3 py-2 bg-teal-500"
-                  onPress={() => setModalVisible(!modalVisible)}
-                >
-                  <Text className="text-white font-medium text-center">
-                    利用する
-                  </Text>
-                </Pressable>
-
-                <Pressable
-                  className="rounded-lg px-3 py-2 bg-gray-500"
-                  onPress={() => setModalVisible(!modalVisible)}
-                >
-                  <Text className="text-white font-medium text-center">
-                    閉じる
-                  </Text>
-                </Pressable>
-              </View>
-            </View>
-          </View>
-        </Modal>
-      </View>
-
+      <SkipModal
+        skipModalVisible={skipModalVisible}
+        setSkipModalVisible={setSkipModalVisible}
+        restOfSkipCount={restOfSkipCount}
+      />
       <TouchableOpacity
         onPress={handlePress}
-        className="bg-teal-300  rounded-full w-16 h-16  shadow-lg p-2 items-center"
+        disabled={isDisabled} // disabled 状態を設定
+        className={`rounded-full w-16 h-16  shadow-lg p-2 items-center ${
+          isDisabled ? "bg-gray-400" : "bg-teal-300"
+        }`} // 条件に応じて背景色を変更
       >
-        <Ionicons name="flag" size={24} color="white" />
-        <Text className=" text-white text-xs font-bold">Skip</Text>
+        <Ionicons
+          name="flag"
+          size={24}
+          color={isDisabled ? "#d3d3d3" : "white"}
+        />
+        <Text
+          className={`${
+            isDisabled ? "text-gray-300" : "text-white"
+          } text-xs font-bold`}
+        >
+          {isDisabled ? "利用済" : " Skip"}
+        </Text>
       </TouchableOpacity>
     </>
   );
